0ba8a7cb1d17ed64acb1f1139367dd9ed7d53e2e
DiyaJohn007 <jdiyamol007@gmail.com>
2022-03-03 09:12:25 +0530

    commit file

--- src/main/java/mavensPackage/MavenClass.java
+++ src/main/java/mavensPackage/MavenClass.java
@@ -4,7 +4,7 @@ public class MavenClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       System.out.println("Hi world,hello");
+       System.out.println("Hi world,hello,Hi");
 	}
 
 }
